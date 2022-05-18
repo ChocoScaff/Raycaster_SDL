e1f563ecdd1168772444a2d4d2ba1426ff0510cb
ChocoScaff <anjul2@hotmail.fr>
2022-05-19 00:30:56 +0200

    add rays

--- raycaster.cpp
+++ raycaster.cpp
@@ -7,7 +7,7 @@
 #define PI 3.14159
 #define WidthTexture 40
 #define HeightTexture 80
-#define ScreenDistance 200
+#define ScreenDistance 300
 
 RayCaster::RayCaster()
 {
@@ -56,28 +56,60 @@ void RayCaster::RayCast(SDL_Renderer *render,int x,int y)
 	if (key[SDL_SCANCODE_Q])
 	{
 		Pa+=0.1; if (Pa > 2*PI) Pa=0;
-		printf("%f\n",Pa);
+		//printf("%f\n",Pa);
 	}
 	if (key[SDL_SCANCODE_E])
 	{
 		Pa-=0.1; if (Pa < 0) Pa=2*PI;
-		printf("%f\n",Pa);
+		//printf("%f\n",Pa);
 	}
 	
 	SDL_Color color = {255, 255, 255, 255};
 
 	if(0 != SDL_SetRenderDrawColor(render, color.r,color.g, color.b, color.a))
    	{
-        	fprintf(stderr,"Error SDL_SetRenderDrawColor : %s",SDL_GetError());
+      	 	fprintf(stderr,"Error SDL_SetRenderDrawColor : %s",SDL_GetError());
 	}
    	SDL_Point point[2];
-	//for (int i=1;i<2;i++)
-	//{
+	point[0].x = x;
+	point[0].y = y;
+	point[1].x = sin(Pa)*ScreenDistance+x; 
+	point[1].y = cos(Pa)*ScreenDistance+y;
+	SDL_RenderDrawLines(render, point, 2);
+	
+	color = {0, 255, 0, 255};
+
+	if(0 != SDL_SetRenderDrawColor(render, color.r,color.g, color.b, color.a))
+   	{
+      	 	fprintf(stderr,"Error SDL_SetRenderDrawColor : %s",SDL_GetError());
+	}
+	for (double i=0.1;i<0.5;i=i+0.1)
+	{
+
 		point[0].x = x;
 		point[0].y = y;
-		point[1].x = sin(Pa)*ScreenDistance+x; //sin(Pa)*360+ScreenDistance+x;
-		point[1].y = cos(Pa)*ScreenDistance+y;
+		point[1].x = sin(Pa-i)*ScreenDistance+x;
+		point[1].y = cos(Pa-i)*ScreenDistance+y;
 		SDL_RenderDrawLines(render, point, 2);
-	//}
+
+	}
+
+	color = {255, 0, 0, 255};
+
+	if(0 != SDL_SetRenderDrawColor(render, color.r,color.g, color.b, color.a))
+   	{
+      	 	fprintf(stderr,"Error SDL_SetRenderDrawColor : %s",SDL_GetError());
+	}
+
+	for (double i=0.1;i<0.5;i=i+0.1)
+	{
+
+		point[0].x = x;
+		point[0].y = y;
+		point[1].x = sin(Pa+i)*ScreenDistance+x;
+		point[1].y = cos(Pa+i)*ScreenDistance+y;
+		SDL_RenderDrawLines(render, point, 2);
+
+	}
 	SDL_RenderPresent(render);
 }
